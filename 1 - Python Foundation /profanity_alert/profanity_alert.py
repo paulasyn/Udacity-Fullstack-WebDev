f0b2fb2fd4e6d2dcb8fd46424e6f8e22c5f60ffe
Paul Asyn <paulasyn@gmail.com>
2018-04-08 01:20:25 -0500

    didnt work on url request

--- 1 - Python Foundation /profanity_alert/profanity_alert.py
+++ 1 - Python Foundation /profanity_alert/profanity_alert.py
@@ -1,9 +1,19 @@
+# detect profanity in text
+import urllib.request
 
 def read_text():
     quotes = open("/Users/paulasyn/Desktop/Udacity-Fullstack-WebDev/1 - Python Foundation /profanity_alert/movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
 
+
+def check_profanity(text_to_check):
+    connection = urllib.request.urlretrieve("http://www.wdylike.appspot.com/?q=" + text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
 
 read_text()
+
